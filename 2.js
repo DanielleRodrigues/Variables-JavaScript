121386b0973cc734b13cf59d53af0cef06fc7a65
daniellerodrigues <daniellerodrigues-14@live.com>
2022-09-08 16:43:31 -0300

    exercicio plataforma generation

---
+++ 2.js
@@ -0,0 +1,26 @@
+const prompt  =  require('prompt-sync')();
+const username = prompt('Insira seu nome: ');
+const altura = prompt('Insira seu altura: ');
+const peso = prompt('Insira seu peso: ');
+const dataInternacao = prompt('Insira seu data de internação: ');
+const infodiabete = prompt('Paciente diábetico? V ou F ');
+
+console.log(`O nome digitado foi : ${username}`);
+console.log(`A altura digitada foi : ${altura}`);
+console.log(`O peso digitado foi : ${peso}`);
+console.log(`O nome digitado foi : ${dataInternacao}`);
+console.log(`Diabete: ${infodiabete}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
